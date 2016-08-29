f717b9378804122a9f08b0a3c942c5ac57e712b3
sideffect0 <mail3renjith@gmail.com>
2016-08-29 11:10:43 +0530

    detect mainloop

--- genpass.py
+++ genpass.py
@@ -2,7 +2,8 @@ import hashlib
 import six
 import argparse
 
-parser = argparse.ArgumentParser(description="Genpass: Generates passwords to use in IRL")
-parser.add_argument("--algo", default="sha512", help="Algorithm to use for example: sha512, random")
-parser.add_argument("--backend", default="stdout", help="Saving Backend to use for (default = stdout)")
-args = parser.parse_args()
+if __name__== '__main__':
+    parser = argparse.ArgumentParser(description="Genpass: Generates passwords to use in IRL")
+    parser.add_argument("--algo", default="sha512", help="Algorithm to use for example: sha512, random")
+    parser.add_argument("--backend", default="stdout", help="Saving Backend to use for (default = stdout)")
+    args = parser.parse_args()
