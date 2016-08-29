4f296626d5f0ff35a5c55b89c2d544de8b794e95
sideffect0 <mail3renjith@gmail.com>
2016-08-29 11:05:26 +0530

    add genpass

--- genpass.py
+++ genpass.py
@@ -1,5 +1,8 @@
 import hashlib
 import six
+import argparse
 
-hash = hashlib.new("sha512")
-hash.update(six.b("sha512"))
+parser = argparse.ArgumentParser(description="Genpass: Generates passwords to use in IRL")
+parser.add_argument("--algo", default="sha512", help="Algorithm to use for example: sha512, random")
+parser.add_argument("--backend", default="stdout", help="Saving Backend to use for (default = stdout)")
+args = parser.parse_args()
